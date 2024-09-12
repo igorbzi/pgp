6923f971985ac03ecae64a0db8762132da8c7f21
Igor Bazei <igorbazei.ilb@gmail.com>
2024-09-12 17:08:13 -0300

    add: scripts sql - banco e tabela users

---
+++ scripts/banco.sql
@@ -0,0 +1,12 @@
+create database pgp;
+
+create table users(
+    cpf varchar(15) not null,
+    username varchar(255) not null,
+    user_password varchar(12) not null,
+    user_email varchar(50) unique not null,
+    user_phone varchar(15) unique not null,
+    user_phone2 varchar(15),
+    user_address varchar(255) not null,
+    primary key(cpf)
+);
