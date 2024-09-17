a9e03dba64e20b7aaee62ef8cade09b9dc6c252b
Igor Bazei <igorbazei.ilb@gmail.com>
2024-09-17 15:18:26 -0300

    add:post para users

--- src/server/server.js
+++ src/server/server.js
@@ -12,24 +12,48 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(3001 , (error)=>{
-    if(error) throw error
-    console.log('Servidor Rodando na porta 3001 ')
+  if(error) throw error
+  console.log('Servidor Rodando na porta 3001 ')
 })
 
 app.get("/" , (req, res)=>{
-    res.send('Bem-vindo no nosso App')
+  res.send('Bem-vindo no nosso App')
 })
 
 /*-----------------users---------------------*/
 app.get("/user", async (req, res)=> {
-    try {
-        const fornecedor = await db.any(
-            "select cpf, username, user_email, user_phone, user_phone2, user_address from users"
-        );
-        res.json(fornecedor).status(200);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(404);
-    }
+  try {
+    const fornecedor = await db.any(
+      "select cpf, username, user_email, user_phone, user_phone2, user_address from users"
+    );
+    res.json(fornecedor).status(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(404);
+  }
 })
 
+app.post("/users", async (req, res) => {
+  try {
+
+    const cpf = req.body.cpf;
+    const nome = req.body.nome;
+    const passwd = req.body.passwd;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const phone2 = req.body.phone2;
+    const address = req.body.address;
+    
+    console.log(`CPF: ${cpf} Nome: ${nome}`);
+
+    db.none(
+      "INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6, $7);",   //passando parâmetros
+      [cpf, nome, passwd, email, phone, phone2, address]
+    );
+    res.sendStatus(200);
+
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+});
