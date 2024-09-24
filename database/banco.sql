e7a647c9cb11af5be65c68887438236abf3df864
Igor Bazei <igorbazei.ilb@gmail.com>
2024-09-24 15:19:32 -0300

    upd: tamanho campo passwd

--- database/banco.sql
+++ database/banco.sql
@@ -3,7 +3,7 @@ create database pgp;
 create table users(
     cpf varchar(15) not null,
     username varchar(255) not null,
-    user_password varchar(12) not null,
+    user_password varchar(100) not null,
     user_email varchar(50) unique not null,
     user_phone varchar(15) unique not null,
     user_phone2 varchar(15),
