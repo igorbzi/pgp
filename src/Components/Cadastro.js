d445f1d17fe231040b0b3ce4b653b335d1708667
Leparfait22 <helomeranda@gmail.com>
2024-10-17 16:39:56 -0300

    melhorando a reponsividade

--- src/Components/Cadastro.js
+++ src/Components/Cadastro.js
@@ -53,6 +53,10 @@ function Login(){
         xs: 'column', 
         sm: 'row', 
       },
+      width: {
+        xs: '100vw', 
+        sm: 'row', 
+      },
 
     }} >
       <Box className='esquerda' sx={{
@@ -65,10 +69,18 @@ function Login(){
                 xs: '30%', 
                 sm: '100%', 
               },
+              
+                marginBottom: {
+                  xs: '2rem', 
+                  sm: '1rem', 
+                }
 
       }}>
-        <h1>QuickFix</h1>
-        <p>Seja Bem Vindo! <br/>Faça sua conta agora mesmo.</p>
+          <h1>QuickFix</h1>
+        <Box>
+          <p>Seja Bem Vindo!</p>
+          <p>Faça sua conta agora mesmo.</p>
+        </Box>
       </Box>
       <Box className='direita'  sx={{
         width: {
@@ -77,135 +89,135 @@ function Login(){
         },
         
       }}>
-        <form onSubmit={handleSubmit} sx={{
-          background: {
-            xs: 'blue', 
-            sm: 'row', 
-          },
-        }} >
-        <Box className='titulo'>
-        <legend>Crie sua Conta</legend>
-        </Box>
-          <input 
-            type="text" 
-            placeholder="Nome" 
-            value={nome} 
-            onChange={(e) => setNome(e.target.value)} 
-          />
-          <Box className='SelectEst'>
-          <select 
-            value={estado} 
-            onChange={(e) => setEstado(e.target.value)}
-          >
-            <option hidden disabled value="">
-              Estado
-            </option>
-            <option value="Acre">Acre</option>
-            <option value="Alagoas">Alagoas</option>
-            <option value="Amapá">Amapá</option>
-            <option value="Amazonas">Amazonas</option>
-            <option value="Bahia">Bahia</option>
-            <option value="Ceará">Ceará</option>
-            <option value="Distrito Federal">Distrito Federal</option>
-            <option value="Espírito Santo">Espírito Santo</option>
-            <option value="Goiás">Goiás</option>
-            <option value="Maranhão">Maranhão</option>
-            <option value="Mato Grosso">Mato Grosso</option>
-            <option value="Mato Grosso do Sul">Mato Grosso do Sul</option>
-            <option value="Minas Gerais">Minas Gerais</option>
-            <option value="Pará">Pará</option>
-            <option value="Paraíba">Paraíba</option>
-            <option value="Paraná">Paraná</option>
-            <option value="Pernambuco">Pernambuco</option>
-            <option value="Piauí">Piauí</option>
-            <option value="Rio de Janeiro">Rio de Janeiro</option>
-            <option value="Rio Grande do Norte">Rio Grande do Norte</option>
-            <option value="Rio Grande do Sul">Rio Grande do Sul</option>
-            <option value="Rondônia">Rondônia</option>
-            <option value="Roraima">Roraima</option>
-            <option value="Santa Catarina">Santa Catarina</option>
-            <option value="São Paulo">São Paulo</option>
-            <option value="Sergipe">Sergipe</option>
-            <option value="Tocantins">Tocantins</option>
-          </select>
+          <Box className='titulo' >
+            <h3>Crie sua Conta</h3>
           </Box>
-          <input 
-            type="email" 
-            placeholder="Email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            required
-            maxLength="50" 
-          />
-          <input 
-            type="text" 
-            placeholder="Cidade" 
-            value={cidade} 
-            onChange={(e) => setCidade(e.target.value)} 
-          />
-          <input 
-            type="password" 
-            placeholder="Senha" 
-            value={senha} 
-            onChange={(e) => setSenha(e.target.value)} 
-          />
-          <input 
-            type="text" 
-            placeholder="Bairro" 
-            value={bairro} 
-            onChange={(e) => setBairro(e.target.value)} 
-          />
-          <input 
-            type="text" 
-            placeholder="CPF" 
-            value={cpf} 
-            onChange={(e) => setCpf(e.target.value)} 
-          />
-          <input 
-            type="text" 
-            placeholder="Logradouro" 
-            value={logradouro} 
-            onChange={(e) => setLogradouro(e.target.value)} 
-          />
-          <input 
-            type="text" 
-            placeholder="Telefone" 
-            value={telefone} 
-            onChange={(e) => setTelefone(e.target.value)} 
-          />
-          <input 
-            type="text" 
-            placeholder="Número" 
-            value={numero} 
-            onChange={(e) => setNumero(e.target.value)} 
-          />
-          <input 
-            type="text" 
-            placeholder="Telefone 2" 
-            value={telefone2} 
-            onChange={(e) => setTelefone2(e.target.value)} 
-          />
-          <input 
-            type="text" 
-            placeholder="Cep" 
-            value={cep} 
-            onChange={(e) => setCep(e.target.value)} 
-          />
-          <select 
-            className='selectTipo' 
-            value={tipo} 
-            onChange={(e) => setTipo(e.target.value)}
-          >
-            <option hidden disabled value="">
-              Cliente ou Prestador
-            </option>
-            <option value="1">Prestador</option>
-            <option value="0">Cliente</option>
-          </select>
-          <button 
-          type="submit"
-          >Cadastrar</button>
-        </form>
+          <Box className="entradas">
+            <form onSubmit={handleSubmit} >
+            <input 
+              type="text" 
+              placeholder="Nome" 
+              value={nome} 
+              onChange={(e) => setNome(e.target.value)} 
+            />
+            
+            <input 
+              type="email" 
+              placeholder="Email" 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+              required
+              maxLength="50" 
+            />
+            
+            <input 
+              type="password" 
+              placeholder="Senha" 
+              value={senha} 
+              onChange={(e) => setSenha(e.target.value)} 
+            />
+            <Box className='cidade_uf'>
+              <input 
+                type="text" 
+                placeholder="Cidade" 
+                value={cidade} 
+                onChange={(e) => setCidade(e.target.value)} 
+              />
+              <select 
+                    value={estado} 
+                    onChange={(e) => setEstado(e.target.value)}
+                  >
+                  <option hidden disabled value="">
+                    Estado
+                  </option>
+                  <option value="Acre">Acre</option>
+                  <option value="Alagoas">Alagoas</option>
+                  <option value="Amapá">Amapá</option>
+                  <option value="Amazonas">Amazonas</option>
+                  <option value="Bahia">Bahia</option>
+                  <option value="Ceará">Ceará</option>
+                  <option value="Distrito Federal">Distrito Federal</option>
+                  <option value="Espírito Santo">Espírito Santo</option>
+                  <option value="Goiás">Goiás</option>
+                  <option value="Maranhão">Maranhão</option>
+                  <option value="Mato Grosso">Mato Grosso</option>
+                  <option value="Mato Grosso do Sul">Mato Grosso do Sul</option>
+                  <option value="Minas Gerais">Minas Gerais</option>
+                  <option value="Pará">Pará</option>
+                  <option value="Paraíba">Paraíba</option>
+                  <option value="Paraná">Paraná</option>
+                  <option value="Pernambuco">Pernambuco</option>
+                  <option value="Piauí">Piauí</option>
+                  <option value="Rio de Janeiro">Rio de Janeiro</option>
+                  <option value="Rio Grande do Norte">Rio Grande do Norte</option>
+                  <option value="Rio Grande do Sul">Rio Grande do Sul</option>
+                  <option value="Rondônia">Rondônia</option>
+                  <option value="Roraima">Roraima</option>
+                  <option value="Santa Catarina">Santa Catarina</option>
+                  <option value="São Paulo">São Paulo</option>
+                  <option value="Sergipe">Sergipe</option>
+                  <option value="Tocantins">Tocantins</option>
+              </select>
+            </Box>
+            <input 
+              type="text" 
+              placeholder="Bairro" 
+              value={bairro} 
+              onChange={(e) => setBairro(e.target.value)} 
+            />
+              <input 
+                type="text" 
+                placeholder="CPF" 
+                value={cpf} 
+                onChange={(e) => setCpf(e.target.value)} 
+              />
+            
+            <input 
+              type="text" 
+              placeholder="Logradouro" 
+              value={logradouro} 
+              onChange={(e) => setLogradouro(e.target.value)} 
+            />
+            <input 
+              type="text" 
+              placeholder="Telefone" 
+              value={telefone} 
+              onChange={(e) => setTelefone(e.target.value)} 
+            />
+            <input 
+              type="text" 
+              placeholder="Número" 
+              value={numero} 
+              onChange={(e) => setNumero(e.target.value)} 
+            />
+            <input 
+              type="text" 
+              placeholder="Telefone 2" 
+              value={telefone2} 
+              onChange={(e) => setTelefone2(e.target.value)} 
+            />
+            <input 
+              type="text" 
+              placeholder="Cep" 
+              value={cep} 
+              onChange={(e) => setCep(e.target.value)} 
+            />
+            <select 
+              className='selectTipo' 
+              value={tipo} 
+              onChange={(e) => setTipo(e.target.value)}
+            >
+              <option hidden disabled value="">
+                Cliente ou Prestador
+              </option>
+              <option value="1">Prestador</option>
+              <option value="0">Cliente</option>
+            </select>
+            <button 
+            type="submit"
+            >Cadastrar</button>
+          </form>
+          </Box>        
       </Box>
     </Box>
   );
