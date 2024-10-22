d1d5b21cc45b247afb6022a7b2b029c9bc5b0519
Igor Bazei <igorbazei.ilb@gmail.com>
2024-10-22 14:35:24 -0300

    add: tabela de serviços

--- database/banco.sql
+++ database/banco.sql
@@ -8,10 +8,23 @@ create table users(
   user_phone varchar(15) unique not null,
   user_phone2 varchar(15),
   user_address varchar(255) not null,
+  user_type smallint not null,
   primary key(cpf)
 );
 
 create table services(
-
+  cod_service serial,
+  service_name varchar(255) not null,
+  service_price numeric(15, 2) not null,
+  service_type integer not null,
+  service_description varchar(600),
+  material_disp boolean,
+  primary key(cod_service),
+  foreign key(service_type) references service_type(cod_type_service)
 )
 
+create table service_type(
+  cod_type_service serial,
+  type_name varchar(255) not null,
+  primary key(cod_type_service)
+)
