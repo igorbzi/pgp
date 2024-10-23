ec9b284f254ff5ea958fb4a73b28d26cb2abb9ff
Leparfait22 <helomeranda@gmail.com>
2024-10-23 15:41:21 -0300

    add: Backend para cadastro dos servicos

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bcrypt = require('bcrypt');
+const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 const pgp = require("pg-promise")({});
 const usuario = "postgres";
@@ -12,17 +12,17 @@ app.use(cors());
 
 app.use(express.json());
 
-app.listen(3001 , (error)=>{
-  if(error) throw error
-  console.log('Servidor Rodando na porta 3001 ')
-})
+app.listen(3001, (error) => {
+  if (error) throw error;
+  console.log("Servidor Rodando na porta 3001 ");
+});
 
-app.get("/" , (req, res)=>{
-  res.send('Bem-vindo no nosso App')
-})
+app.get("/", (req, res) => {
+  res.send("Bem-vindo no nosso App");
+});
 
 /*-----------------users---------------------*/
-app.get("/users", async (req, res)=> {
+app.get("/users", async (req, res) => {
   try {
     const user = await db.any(
       "select cpf, username, user_email, user_phone, user_phone2, user_address from users"
@@ -33,11 +33,10 @@ app.get("/users", async (req, res)=> {
     console.log(error);
     res.sendStatus(404);
   }
-})
+});
 
 app.post("/users", async (req, res) => {
   try {
-
     const saltRounds = 10;
     const cpf = req.body.cpf;
     const nome = req.body.nome;
@@ -53,39 +52,40 @@ app.post("/users", async (req, res) => {
     const unique_cpf = await db.oneOrNone(
       "SELECT 1 from users where cpf = $1",
       [cpf]
-    )
+    );
 
     const unique_email = await db.oneOrNone(
       "SELECT 1 from users where user_email = $1",
       [email]
-    )
+    );
 
     const unique_phone = await db.oneOrNone(
       "SELECT 1 from users where user_phone = $1",
       [phone]
-    )
-    
-    if(unique_cpf) {
+    );
+
+    if (unique_cpf) {
       res.status(400).send("CPF já cadastrado");
       return;
-    } else if (unique_email){
+    } else if (unique_email) {
       res.status(400).send("Email já cadastrado");
       return;
-    } else if(unique_phone){
+    } else if (unique_phone) {
       res.status(400).send("Telefone principal já cadastrado");
       return;
     }
 
     db.none(
-      "INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6, $7, $8);",   //passando parâmetros
+      "INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", //passando parâmetros
       [cpf, nome, passwd, email, phone, phone2, address, type]
-    ).then(()=>{
-      res.sendStatus(200);
-    }).catch(error => {
-      res.status(400).send("Preencha todos os campos!");
-      return
-    })
-
+    )
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((error) => {
+        res.status(400).send("Preencha todos os campos!");
+        return;
+      });
   } catch (error) {
     console.log(error);
     res.status(400).send("Preencha todos os campos obrigatórios!");
@@ -93,27 +93,149 @@ app.post("/users", async (req, res) => {
 });
 
 app.delete("/users", async (req, res) => {
-  try{
-
+  try {
     const id = req.body.cpf; //pega parametro da req
 
-    const exist = await db.oneOrNone(
-      "select 1 from users where cpf = $1",
-      [id]
-    )
+    const exist = await db.oneOrNone("select 1 from users where cpf = $1", [
+      id,
+    ]);
 
-    if(!exist){
+    if (!exist) {
       res.status(400).send("Usuário não cadastrado");
       return;
     }
 
     db.none(
-        "DELETE from users where cpf = $1;", [id] //deleta pelo cpf
+      "DELETE from users where cpf = $1;",
+      [id] //deleta pelo cpf
     );
     res.sendStatus(200);
-
   } catch (error) {
     console.log(error);
-    res.sendStatus(400)
+    res.sendStatus(400);
   }
 });
+
+/*-----------------Services---------------------*/
+app.get("/services", async (req, res) => {
+  try {
+    const services = await db.any("select * from services");
+    //const user = await db.any(
+    //"select cpf, username, user_email, user_phone, user_phone2, user_address from users"
+    //);
+    res.json(services).status(200);
+    console.log(services);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(404);
+  }
+});
+
+app.post("/services", async (req, res) => {
+  try {
+    const service_name = req.body.service_name;
+    const service_description = req.body.service_description;
+    const service_price = req.body.service_price;
+    const service_type = req.body.service_type;
+    const material_disp = req.body.material_disp;
+
+    const service_category = await db.oneOrNone(
+      "SELECT cod_type_service from service_type where type_name = $1",
+      [service_type]
+    );
+
+    console.log(service_category);
+
+    await db
+      .none(
+        "INSERT INTO services (service_name, service_price, service_type, service_description, material_disp) VALUES ($1, $2, $3, $4, $5);", //passando parâmetros
+        [service_name,service_price,service_category.cod_type_service,service_description,material_disp]
+      )
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((error) => {
+        res.status(400).send(error);
+        console.log(error);
+        return;
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+app.delete("/services", async (req, res) => {
+  try {
+    const id = req.body.cod_service;
+    console.log(id);
+
+    const exist = await db.oneOrNone(
+      "select 1 from services where cod_service = $1",
+      [id]
+    );
+
+    if (!exist) {
+      res.status(400).send("Serviço não cadastrado");
+      return;
+    }
+
+    await db.none(
+      "DELETE from services where cod_service = $1;",
+      [id] 
+    );
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+});
+
+
+app.put("/services", async (req, res) => {
+  try {
+    const id = req.body.cod_service;
+    const service_name = req.body.service_name;
+    const service_description = req.body.service_description;
+    const service_price = req.body.service_price;
+    const service_type = req.body.service_type;
+    const material_disp = req.body.material_disp;
+
+    console.log(id);
+    
+
+    const exist = await db.oneOrNone(
+      "select 1 from services where cod_service = $1",
+      [id]
+    );
+    if(!exist){
+      res.status(400).send("Serviço não encontrado");
+      return;
+    }
+
+    const service_category = await db.oneOrNone(
+      "SELECT cod_type_service from service_type where type_name = $1",
+      [service_type]
+    );
+    
+    console.log(service_category);
+
+    await db
+      .none(
+        "UPDATE services SET service_name = $1, service_price = $2, service_type = $3, service_description = $4, material_disp = $5 WHERE cod_service = $6;", 
+        [service_name,service_price,service_category.cod_type_service,service_description,material_disp,id]
+      )
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((error) => {
+        res.status(400).send(error);
+        console.log(error);
+        return;
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+}
+);
